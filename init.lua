84fb7b8154add966c84317b44d7cd55ace61b5fa
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2020-06-13 02:27:51 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -34,7 +34,7 @@ minetest.register_chatcommand("rank", {
 		player:set_attribute("rank_name", param2)
 	end
 	
-	if param3 == "blue" or param3 == "red" or param == "yellow" or param == "green" or param == "black" or param == "white" then
+	if param3 == "blue" or param3 == "red" or param3 == "yellow" or param3 == "green" or param3 == "black" or param3 == "white" then
 		player:set_attribute("rank_color", param3)
 	else
 		return
