725ac87919676cb10f244b88870bc4523d0f1ff2
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2020-07-13 07:57:55 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,17 +1,30 @@
+rank_color = {
+	red = "#FF0000",
+	blue = "#0000FF",
+	yellow = "#FFFF00",
+	green = "#008000",
+	black = "#000000",
+	white = "#FFFFFF"
+}
+
+--Can Be renamed if you want 
+
 minetest.register_on_joinplayer(function(player)
 	if player:get_attribute("rank_color") == nil then
 		player:set_attribute("rank_color", "green")
 	end
-	
+	local name = player:get_player_name()
+	if player:get_attribute("rank_color") ~= nil and player:get_attribute("rank_color") ~= nil then
+		player:set_nametag_attributes({text = ""..minetest.colorize(rank_color[player:get_attribute("rank_color")], player:get_attribute("rank_name")).." "..name})
+	end
 	if player:get_attribute("rank_name") == nil then
 		player:set_attribute("rank_name", "Player")
 	end
 end)
 
 
-
-
 minetest.register_privilege("rank_master", {description = "Can create ranks and also grant them"})
+
 
 minetest.register_chatcommand("rank", {
 	description = "Create a rank and give player a rank",
@@ -22,37 +35,30 @@ minetest.register_chatcommand("rank", {
 	local param3 = param:split(' ')[3]
 	local player = minetest.get_player_by_name(param1)
 	
-	if not player then
-		return
-		minetest.chat_send_player(name, "player is not online")
-	end
+		if not player then
+			return
+			minetest.chat_send_player(name, "player is not online")
+		end
 	
-	if param2 == "" then
-		return
-		minetest.chat_send_player(name, "rank name is missing")
-	else
-		player:set_attribute("rank_name", param2)
-	end
+		if param2 == nil then
+			return
+			minetest.chat_send_player(name, "rank name is missing")
+		else
+			player:set_attribute("rank_name", param2)
+		end
 	
-	if param3 == "blue" or param3 == "red" or param3 == "yellow" or param3 == "green" or param3 == "black" or param3 == "white" then
-		player:set_attribute("rank_color", param3)
-	else
-		return
-		minetest.chat_send_player(name, "Invaild rank color you can use red, blue, yellow, green, black, white") 
-	end
+		if param3 == "blue" or param3 == "red" or param3 == "yellow" or param3 == "green" or param3 == "black" or param3 == "white" then
+			player:set_attribute("rank_color", param3)
+		else
+			return
+			minetest.chat_send_player(name, "Invaild rank color you can use red, blue, yellow, green, black, white") 
+		end
+		if param3 == "blue" or param3 == "red" or param3 == "yellow" or param3 == "green" or param3 == "black" or param3 == "white" and param2 ~= nil then
+			player:set_nametag_attributes({text = ""..minetest.colorize(rank_color[player:get_attribute("rank_color")], player:get_attribute("rank_name")).." "..name})
+			minetest.chat_send_player(name, "Rank has been updated")
+		end
 	end
 })
-
-
-
-rank_color = {
-	red = "#FF0000",
-	blue = "#0000FF",
-	yellow = "#FFFF00",
-	green = "#008000",
-	black = "#000000",
-	white = "#FFFFFF"
-}
 
 
 minetest.register_on_chat_message(function(name, message)
